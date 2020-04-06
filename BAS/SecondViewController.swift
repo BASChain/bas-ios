167c384e3b7bce97aff43d60b18e67e6bbd780e5
hyperorchid <hyperorchid@outlook.com>
2020-04-06 12:18:02 +0800

    init

---
+++ BAS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BAS
+//
+//  Created by hyperorchid on 2020/4/6.
+//  Copyright © 2020 com.baschain. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
